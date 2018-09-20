81e6f974c0e46e037b7f9edbc1abb7146dee7a85
Jérémie Zarca <jeremie.zarca@gmail.com>
2018-09-20 11:21:34 +0200

    initial commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const bodyParser = require('body-parser');
 const fetchCookie = require('./subway')
 
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 
 app.use(bodyParser.urlencoded({
   extended: true
